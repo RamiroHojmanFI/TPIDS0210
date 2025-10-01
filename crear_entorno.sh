2de028aae84d5fc09f9011c77a03e4d99d0fbb9d
braian46coder <braian46r@gmail.com>
2025-10-01 15:05:12 -0300

    Archivo para crear entorno

---
+++ crear_entorno.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir TP2-IDS
+
+cd TP2-IDS
+
+mkdir .venv
+
+mkdir static
+
+mkdir templates
+
+touch app.py
+
+cd static
+
+mkdir css
+
+mkdir images
+
+echo "Se ha creado el entorno."
